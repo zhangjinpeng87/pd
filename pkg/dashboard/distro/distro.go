b2ade8cdf9ee47f6e50ae86bdf2b309808a3363b
Suhaha <jklopsdfw@gmail.com>
2021-09-06 20:16:58 +0800

    dashboard: update TiDB Dashboard to v2021.09.03.1 (#4076)
    Signed-off-by: Suhaha <jklopsdfw@gmail.com>
    
    Co-authored-by: 混沌DM <hundundm@gmail.com>

--- pkg/dashboard/distro/distro.go
+++ pkg/dashboard/distro/distro.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build dashboard_distro
+// +build dashboard_distro
+
 package distro
 
 import (
--- pkg/dashboard/distro/placeholder.go
+++ pkg/dashboard/distro/placeholder.go
@@ -11,11 +11,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// +build !dashboard_distro
 
 package distro
-
-import "github.com/pingcap/tidb-dashboard/pkg/utils/distro"
-
-// Resource declared the distro brand information
-var Resource = distro.Resource

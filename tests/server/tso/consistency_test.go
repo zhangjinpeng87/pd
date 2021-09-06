b86da74ed837923fe9e28936176598729de09d5d
混沌DM <hundundm@gmail.com>
2021-09-06 22:54:57 +0800

    *: support go:build lines for go1.17 (#4086)
    Signed-off-by: HunDunDM <hundundm@gmail.com>

--- tests/server/tso/consistency_test.go
+++ tests/server/tso/consistency_test.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build tso_full_test || tso_consistency_test
 // +build tso_full_test tso_consistency_test
 
 package tso_test

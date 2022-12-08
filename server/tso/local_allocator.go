268c2b675a6a65df30819e7421ded8f5d98c83c7
Ryan Leung <rleungx@gmail.com>
2022-12-08 14:52:04 +0800

    pkg: move the election to pkg (#5767)
    ref tikv/pd#5766
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/tso/local_allocator.go
+++ server/tso/local_allocator.go
@@ -22,10 +22,10 @@ import (
 
 	"github.com/pingcap/kvproto/pkg/pdpb"
 	"github.com/pingcap/log"
+	"github.com/tikv/pd/pkg/election"
 	"github.com/tikv/pd/pkg/errs"
 	"github.com/tikv/pd/pkg/tsoutil"
 	"github.com/tikv/pd/pkg/typeutil"
-	"github.com/tikv/pd/server/election"
 	"go.etcd.io/etcd/clientv3"
 	"go.uber.org/zap"
 )

49fe21e05baeaa9c33c8198251842b9fd97e856f
Ryan Leung <rleungx@gmail.com>
2023-01-09 11:20:22 +0800

    *: fix hot region storage test (#5835)
    close tikv/pd#5828
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/storage/hot_region_storage.go
+++ server/storage/hot_region_storage.go
@@ -227,13 +227,13 @@ func (h *HotRegionStorage) backgroundFlush() {
 	}
 }
 
-// NewIterator return a iterator which can traverse all data as reqeust.
+// NewIterator return a iterator which can traverse all data as request.
 func (h *HotRegionStorage) NewIterator(requireTypes []string, startTime, endTime int64) HotRegionStorageIterator {
 	iters := make([]iterator.Iterator, len(requireTypes))
 	for index, requireType := range requireTypes {
 		requireType = strings.ToLower(requireType)
 		startKey := HotRegionStorePath(requireType, startTime, 0)
-		endKey := HotRegionStorePath(requireType, endTime, math.MaxInt64)
+		endKey := HotRegionStorePath(requireType, endTime, math.MaxUint64)
 		iter := h.LevelDBKV.NewIterator(&util.Range{Start: []byte(startKey), Limit: []byte(endKey)}, nil)
 		iters[index] = iter
 	}

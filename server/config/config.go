1bc8e2a5b4a69bad7146cd50105add5952ca3e0e
ShuNing <nolouch@gmail.com>
2023-02-13 18:26:01 +0800

    dashboard: default close telemetry (#5974)
    close tikv/pd#5973
    
    dashboard: default close telemetry
    
    Co-authored-by: lhy1024 <admin@liudos.us>
    Co-authored-by: Ryan Leung <rleungx@gmail.com>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- server/config/config.go
+++ server/config/config.go
@@ -284,7 +284,7 @@ const (
 )
 
 var (
-	defaultEnableTelemetry = true
+	defaultEnableTelemetry = false
 	defaultRuntimeServices = []string{}
 	defaultLocationLabels  = []string{}
 	// DefaultStoreLimit is the default store limit of add peer and remove peer.
@@ -423,10 +423,6 @@ func (c *Config) Parse(arguments []string) error {
 		}
 		if meta.IsDefined("schedule", "disable-raft-learner") {
 			msg := fmt.Sprintf("disable-raft-learner in %s is deprecated", c.configFile)
-			c.WarningMsgs = append(c.WarningMsgs, msg)
-		}
-		if meta.IsDefined("dashboard", "disable-telemetry") {
-			msg := fmt.Sprintf("disable-telemetry in %s is deprecated, use enable-telemetry instead", c.configFile)
 			c.WarningMsgs = append(c.WarningMsgs, msg)
 		}
 	}
@@ -1424,8 +1420,6 @@ type DashboardConfig struct {
 	InternalProxy      bool   `toml:"internal-proxy" json:"internal-proxy"`
 	EnableTelemetry    bool   `toml:"enable-telemetry" json:"enable-telemetry"`
 	EnableExperimental bool   `toml:"enable-experimental" json:"enable-experimental"`
-	// WARN: DisableTelemetry is deprecated.
-	DisableTelemetry bool `toml:"disable-telemetry" json:"disable-telemetry,omitempty"`
 }
 
 // ToTiDBTLSConfig generates tls config for connecting to TiDB, used by tidb-dashboard.
@@ -1449,7 +1443,6 @@ func (c *DashboardConfig) adjust(meta *configutil.ConfigMetaData) {
 	if !meta.IsDefined("enable-telemetry") {
 		c.EnableTelemetry = defaultEnableTelemetry
 	}
-	c.EnableTelemetry = c.EnableTelemetry && !c.DisableTelemetry
 }
 
 // ReplicationModeConfig is the configuration for the replication policy.

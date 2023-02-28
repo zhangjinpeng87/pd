b7e47c5e1fa05bada7655e35d1ae1a620f45a048
glorv <glorvs@163.com>
2023-02-28 12:25:08 +0800

    test: fix unstable test TestClientWatchWithRevision (#6056)
    close tikv/pd#6013
    
    Signed-off-by: glorv <glorvs@163.com>

--- tests/client/global_config_test.go
+++ tests/client/global_config_test.go
@@ -290,7 +290,7 @@ func (suite *globalConfigTestSuite) TestClientWatchWithRevision() {
 	res, revision, err := suite.client.LoadGlobalConfig(suite.server.Context(), nil, globalConfigPath)
 	suite.NoError(err)
 	suite.Len(res, 1)
-	suite.Equal(r.Header.GetRevision(), revision)
+	suite.LessOrEqual(r.Header.GetRevision(), revision)
 	suite.Equal(pd.GlobalConfigItem{EventType: pdpb.EventType_PUT, Name: suite.GetEtcdPath("test"), PayLoad: []byte("test"), Value: "test"}, res[0])
 	// Mock when start watcher there are existed some keys, will load firstly
 	for i := 0; i < 6; i++ {

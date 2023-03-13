a33228e12eb227f96c08b8e10dafe6006b178c1a
Nathan <ethercflow@gmail.com>
2023-03-13 12:20:38 +0800

    rule_checker: revert promote witness when has pending voter (#6139)
    ref tikv/pd#5627
    
    rule_checker: revert promote witness when has pending voter
    
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- pkg/schedule/checker/rule_checker.go
+++ pkg/schedule/checker/rule_checker.go
@@ -205,13 +205,6 @@ func (c *RuleChecker) fixRulePeer(region *core.RegionInfo, fit *placement.Region
 			ruleCheckerReplaceOfflineCounter.Inc()
 			return c.replaceUnexpectRulePeer(region, rf, fit, peer, offlineStatus)
 		}
-
-		if c.isWitnessEnabled() && c.isPendingVoter(region, peer) {
-			if witness, ok := c.hasAvailableWitness(region, peer); ok {
-				ruleCheckerPromoteWitnessCounter.Inc()
-				return operator.CreateNonWitnessPeerOperator("promote-witness-for-pending", c.cluster, region, witness)
-			}
-		}
 	}
 	// fix loose matched peers.
 	for _, peer := range rf.PeersWithDifferentRole {
@@ -500,10 +493,6 @@ func (c *RuleChecker) isOfflinePeer(peer *metapb.Peer) bool {
 		return false
 	}
 	return !store.IsPreparing() && !store.IsServing()
-}
-
-func (c *RuleChecker) isPendingVoter(region *core.RegionInfo, peer *metapb.Peer) bool {
-	return region.GetPendingVoter(peer.Id) != nil
 }
 
 func (c *RuleChecker) hasAvailableWitness(region *core.RegionInfo, peer *metapb.Peer) (*metapb.Peer, bool) {

b5119ea4bf2c3bc1d94256810c7e3e3670e96f45
lucasliang <nkcs_lykx@hotmail.com>
2023-11-10 16:32:12 +0800

    scheduler: refine the interval of scheduling tick in evict-slow-trend-scheduler. (#7326)
    ref tikv/pd#7156
    
    Implement the `GetNextInterval` for `evict-slow-trend-scheduler`, to refine the ticking interval.
    
    Default `GetNextInterval` is not appropriate for `evict-slow-trend-scheduler`, as it might delay
    the checking of other nodes' slowness status.
    
    This pr adjusts the ticking interval of the evict-slow-trend-scheduler to optimize
    its behavior. If a slow node is already identified as a candidate, the next
    interval is now set to be shorter, ensuring quicker subsequent scheduling. 
    This refinement aims to decrease response time.
    
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- pkg/schedule/schedulers/evict_slow_trend.go
+++ pkg/schedule/schedulers/evict_slow_trend.go
@@ -108,8 +108,12 @@ func (conf *evictSlowTrendSchedulerConfig) getKeyRangesByID(id uint64) []core.Ke
 	return []core.KeyRange{core.NewKeyRange("", "")}
 }
 
+func (conf *evictSlowTrendSchedulerConfig) hasEvictedStores() bool {
+	return len(conf.EvictedStores) > 0
+}
+
 func (conf *evictSlowTrendSchedulerConfig) evictedStore() uint64 {
-	if len(conf.EvictedStores) == 0 {
+	if !conf.hasEvictedStores() {
 		return 0
 	}
 	// If a candidate passes all checks and proved to be slow, it will be
@@ -235,6 +239,19 @@ type evictSlowTrendScheduler struct {
 	*BaseScheduler
 	conf    *evictSlowTrendSchedulerConfig
 	handler http.Handler
+}
+
+func (s *evictSlowTrendScheduler) GetNextInterval(interval time.Duration) time.Duration {
+	var growthType intervalGrowthType
+	// If it already found a slow node as candidate, the next interval should be shorter
+	// to make the next scheduling as soon as possible. This adjustment will decrease the
+	// response time, as heartbeats from other nodes will be received and updated more quickly.
+	if s.conf.hasEvictedStores() {
+		growthType = zeroGrowth
+	} else {
+		growthType = exponentialGrowth
+	}
+	return intervalGrow(s.GetMinInterval(), MaxScheduleInterval, growthType)
 }
 
 func (s *evictSlowTrendScheduler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
